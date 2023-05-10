c856bf3c1e1dae180a7c426ac09b4b2accf2395f
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-05-10 15:08:52 +0900

    feat: nutrientController body로 받기로 수정

--- makeat_be/src/main/java/io/makeat/makeat_be/controller/NutritionController.java
+++ makeat_be/src/main/java/io/makeat/makeat_be/controller/NutritionController.java
@@ -1,9 +1,11 @@
 package io.makeat.makeat_be.controller;
 
+import io.makeat.makeat_be.dto.FoodDto;
 import io.makeat.makeat_be.service.NutritionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,9 @@ public class NutritionController {
     private final NutritionService ns;
 
     @GetMapping("/api/{foodName}/{quentity}/{quentityType}")
-    public String getNutritionInfo(@PathVariable("foodName") String foodName, @PathVariable("quentity") Float quentity, @PathVariable("quentityType") String quentityType) throws IOException, ParseException {
+    public String getNutritionInfo(@ModelAttribute FoodDto food) throws IOException, ParseException {
 
-        String nutrientInfo = ns.getNutrient(foodName, quentity, quentityType);
+        String nutrientInfo = ns.getNutrient(food.getFoodName(), food.getQuentity(), food.getQuentityType());
 
         return nutrientInfo;
 
--- makeat_be/src/main/java/io/makeat/makeat_be/dto/FoodDto.java
+++ makeat_be/src/main/java/io/makeat/makeat_be/dto/FoodDto.java
@@ -5,5 +5,6 @@ import lombok.Data;
 @Data
 public class FoodDto {
     private String foodName;
-    private float capacity;
+    private Float quentity;
+    private String quentityType;
 }
