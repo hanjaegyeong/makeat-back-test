eb478a01870069c6f597c43f0aa987a68b0926d4
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-05-10 20:35:27 +0900

    feat: FlaskToFlutterController 작성

---
+++ makeat_be/src/main/java/io/makeat/makeat_be/controller/FlaskToFlutterController.java
@@ -0,0 +1,38 @@
+package io.makeat.makeat_be.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+@RestController
+public class FlaskToFlutterController {
+
+    @Autowired
+    private WebClient webClient;
+
+    /**
+     * 플라스크에서 받은 값 플러터로 전송하는 함수
+     * @param abc
+     * @return
+     */
+    @PostMapping(value = "/sendToFlutter", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> sendToFlutter(@RequestBody int abc) {
+        // 플러터로 값을 전송하는 코드
+        String flutterUrl = "http://<flutter_ip>:<flutter_port>/receiveFromSpring";
+        // WebClient를 사용하여 플러터로 값을 전송합니다.
+        webClient.post()
+                .uri(flutterUrl)
+                .body(BodyInserters.fromValue(abc))
+                .retrieve()
+                .toBodilessEntity()
+                .block();
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
