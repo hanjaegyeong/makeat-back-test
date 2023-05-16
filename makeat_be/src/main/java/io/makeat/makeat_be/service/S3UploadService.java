728f342bfa42c4537b434c1e2b92cd38b7e7ee85
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-05-16 17:44:51 +0900

    feat: S3UploadService content-type image-png로 수정

--- makeat_be/src/main/java/io/makeat/makeat_be/service/S3UploadService.java
+++ makeat_be/src/main/java/io/makeat/makeat_be/service/S3UploadService.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -34,7 +35,8 @@ public class S3UploadService {
     @Value("${aws.s3.bucket}")
     private String bucketName;
 
-    public ResponseEntity<Object> uploadImage(MultipartFile file){
+
+    public ResponseEntity<Object> uploadImage(MultipartFile file) {
         String fileName = file.getOriginalFilename();
 
         // S3 클라이언트 객체 생성
@@ -48,7 +50,12 @@ public class S3UploadService {
         try {
             // 파일명 중복 방지를 위해 UUID를 추가
             String uniqueFileName = UUID.randomUUID().toString() + "_" + fileName;
-            PutObjectResult result = s3Client.putObject(new PutObjectRequest(bucketName, uniqueFileName, file.getInputStream(), null));
+
+            // Content-Type 설정을 위한 ObjectMetadata 객체 생성
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("image/png");
+
+            PutObjectResult result = s3Client.putObject(new PutObjectRequest(bucketName, uniqueFileName, file.getInputStream(), metadata));
 
             // 업로드한 파일의 S3 버킷 URL 반환
             String fileUrl = "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + uniqueFileName;
