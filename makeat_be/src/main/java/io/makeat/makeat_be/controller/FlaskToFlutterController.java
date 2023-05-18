7579d7e25ef9336a321cc0e5722d0d95612c4bf7
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-05-18 14:41:47 +0900

    feat: 플투플 컨트롤러 요청값, 반환변수명 수정

--- makeat_be/src/main/java/io/makeat/makeat_be/controller/FlaskToFlutterController.java
+++ makeat_be/src/main/java/io/makeat/makeat_be/controller/FlaskToFlutterController.java
@@ -1,12 +1,15 @@
 package io.makeat.makeat_be.controller;
 
+import io.makeat.makeat_be.dto.NutrientDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
@@ -17,24 +20,24 @@ public class FlaskToFlutterController {
     private WebClient webClient;
 
     /**
-     * 플라스크에서 받은 값 플러터로 전송하는 함수
-     * @param abc
-     * @return
+     * 플러터에서 요청받으면 플라스크에서 받은 영양소 json값 플러터로 전송하는 함수
+     * @param file
+     * @return nutrient json
      */
     @PostMapping(value = "/sendToFlutter", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> sendToFlutter(@RequestBody int abc) {
+    public ResponseEntity<?> sendToFlutter(@RequestParam("file") MultipartFile file) {
         // 플러터로 값을 전송하는 코드
-        String flutterUrl = "http://<flutter_ip>:<flutter_port>/receiveFromSpring";
+        String flutterUrl = "http://<flutter_ip>:5000/image ";
 
         // WebClient를 사용하여 플러터로 값을 전송하고 응답을 받습니다.
-        ResponseEntity<String> response = webClient.post()
+        ResponseEntity<String> nutrient = webClient.post()
                 .uri(flutterUrl)
-                .body(BodyInserters.fromValue(abc))
+                .body(BodyInserters.fromValue(file))
                 .exchange()
                 .flatMap(clientResponse -> clientResponse.toEntity(String.class))
                 .block();
 
         // 응답을 그대로 반환합니다.
-        return response;
+        return nutrient;
     }
 }
