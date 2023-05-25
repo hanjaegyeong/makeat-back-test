9f256c8a264a0b7189d3b508f55d2c8cd977067f
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-05-25 15:26:03 +0900

    feat: 플러터 to 플라스크 클래스 전체로직 수행하도록

--- makeat_be/src/main/java/io/makeat/makeat_be/controller/FlaskToFlutterController.java
+++ makeat_be/src/main/java/io/makeat/makeat_be/controller/FlaskToFlutterController.java
@@ -1,6 +1,8 @@
 package io.makeat.makeat_be.controller;
 
 import io.makeat.makeat_be.dto.NutrientDto;
+import io.makeat.makeat_be.service.S3UploadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,25 +16,30 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
 @RestController
+@RequiredArgsConstructor
 public class FlaskToFlutterController {
 
-    @Autowired
-    private WebClient webClient;
+    private final WebClient webClient;
+    private final S3UploadService ss;
 
     /**
-     * 플러터에서 요청받으면 플라스크에서 받은 영양소 json값 플러터로 전송하는 함수
+     * 플러터에서 image받아서 s3 url 플라스크로 보낸 뒤
+     * 플라스크에서 받은 영양소 json값 플러터로 리턴하는 함수
      * @param image
      * @return nutrient json
      */
     @PostMapping(value = "/sendToFlutter", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> sendToFlutter(@RequestParam("file") MultipartFile image) {
-        // 플러터로 값을 전송하는 코드
-        String flutterUrl = "http://<flutter_ip>:5000/image ";
+    public ResponseEntity<?> sendToFlutter(@RequestParam("image") MultipartFile image) {
 
-        // WebClient를 사용하여 플러터로 값을 전송하고 응답을 받습니다.
+        ResponseEntity<Object> imageURL = ss.uploadImage(image);
+
+        // 플러터로 값을 전송하는 코드
+        String flutterUrl = "http://3.35.9.94:5000/image ";
+
+        // WebClient를 사용하여 플러터로 url을 전송하고 응답을 받습니다.
         ResponseEntity<String> nutrient = webClient.post()
                 .uri(flutterUrl)
-                .body(BodyInserters.fromValue(image))
+                .body(BodyInserters.fromValue(imageURL))
                 .exchange()
                 .flatMap(clientResponse -> clientResponse.toEntity(String.class))
                 .block();
